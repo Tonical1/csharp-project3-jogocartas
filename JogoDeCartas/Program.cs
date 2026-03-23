bc1fa82b3bccee6e7852e6bf3b7072c697590069
Tonical R <errodrigues2006@outlook.com.br>
2026-03-23 14:32:58 -0300

    Creating Program

---
+++ JogoDeCartas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Game;
+
+namespace JogoDeCartas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Core game = new Core();
+            game.Players.Add("Pedro");
+            game.Players.Add("João");
+
+            game.BuildDeck();
+
+            foreach (var card in Core.CardValues)
+            {
+                Console.WriteLine($"a carta {card.Key} tem o valor de: {card.Value}");
+            }
+        }
+    }
+}
