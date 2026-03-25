ca99d32511bb95cec950f6252ff13521c46d2380
labsfiap <labsfiap@PA401MICRO08>
2026-03-25 11:14:28 -0300

    Update Vars

--- GameInfo/Components/PlayerList.cs
+++ GameInfo/Components/PlayerList.cs
@@ -4,16 +4,16 @@ namespace GameLib.Components
 {
     public class PlayerList
     {
-        private List<Player> GameLibList = new List<Player>();
-        public IReadOnlyList<Player> GetPlayers() => GameLibList.AsReadOnly();
+        private List<Player> playerList = new List<Player>();
+        public IReadOnlyList<Player> GetPlayers() => playerList.AsReadOnly();
 
-        private static int MinPlayers = 2;
-        private static int MaxPlayers = 4;
+        public static int MinPlayers = 2;
+        public static int MaxPlayers = 4;
         public bool AddPlayer(Player player)
         {
-            if (!GameLibList.Exists(Target => Target.Name == player.Name))
+            if (!playerList.Exists(Target => Target.Name == player.Name) && playerList.Count<=MaxPlayers)
             {
-                GameLibList.Add(player);
+                playerList.Add(player);
                 return true;
             }
             return false;
@@ -21,7 +21,7 @@ namespace GameLib.Components
 
         public void RemovePlayer(string name)
         {
-            GameLibList.RemoveAll(Target => Target.Name == name);
+            playerList.RemoveAll(Target => Target.Name == name);
         }
     }
 }
