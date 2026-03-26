e922a48b3144598a214c06e3e6b61c4a8060e5e1
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 10:45:48 -0300

    Adicionando Aplication

--- JogoDeCartas/Program.cs
+++ JogoDeCartas/Program.cs
@@ -9,10 +9,6 @@ namespace JogoDeCartas
         {
             Core game = new Core();
 
-            //foreach (var card in Core.CardValues)
-            //{
-            //    Console.WriteLine($"a carta {card.Key} tem o valor de: {card.Value}");
-            //}
         }
     }
 }
